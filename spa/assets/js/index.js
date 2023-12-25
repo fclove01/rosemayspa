2dcd5dc8ef4eb257e2b8dceb9bbf056bed1995ad
kcuong <mr.fx.cuong@gmail.com>
2023-12-25 19:28:11 +0700

    backup

--- spa/assets/js/index.js
+++ spa/assets/js/index.js
@@ -19,33 +19,32 @@ document.addEventListener('DOMContentLoaded',() => {
       if (name != "" && phone != ""){
         btn.classList.add('d-none')
         document.querySelector('.loader').classList.remove('d-none')
-        fetch('/submit-form', {
-            method: "POST", 
-            mode: "cors", 
-            cache: "no-cache", 
-            credentials: "same-origin", 
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data), 
-          })
-        .then(e=>e.json())
-        .then((e) => {
-        if (e.ok){
-            btn.className = "btn btn-success"
-            btn.textContent = "SUCESS"
-            document.querySelector('.loader').classList.add('d-none')
-            btn.classList.remove('d-none')
-            setTimeout(() => {
-                if(btn.className == 'btn btn-success'){
-                    btn.className = 'btn btn-primary'
-                }
-            }, 2000)
-        }
+        const url = 'https://script.google.com/macros/s/AKfycbyejLMtAafFV7nn0goTQYZUiHkfBE45k5MY0mCrN2NlhiAeHvHiJ71X2KlRPaDe44eB/exec'
+        const ctime = new Date()
+        data.ctime=ctime.toLocaleString('vi-VN', { timeZone: 'Asia/Ho_Chi_Minh' })
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          mode: 'no-cors',
+          body: JSON.stringify(data)
         })
-        .catch ((err) => {
-        console.error(err)
+        .then(response => response.text())
+        .then(result => {
+          btn.className = "btn btn-success"
+          btn.textContent = "SUCESS"
+          document.querySelector('.loader').classList.add('d-none')
+          btn.classList.remove('d-none')
+          setTimeout(() => {
+              if(btn.className == 'btn btn-success'){
+                  btn.className = 'btn btn-primary'
+              }
+          }, 2000)
         })
+        .catch(error => {
+            console.error('Error:', error);
+        }); 
       } 
     })
   }
